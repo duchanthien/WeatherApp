b24f0b8f802faaee8fa3177f97023f09ebb0177b
thienduchan <hanthienduc.196@gmail.com>
2017-06-25 10:44:22 +0700

    Setup resources and implmenting main layout

--- WeatherForecast/app/src/main/java/com/hanthienduc/weatherforecast/MainActivity.java
+++ WeatherForecast/app/src/main/java/com/hanthienduc/weatherforecast/MainActivity.java
@@ -2,12 +2,33 @@ package com.hanthienduc.weatherforecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_forecast));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the menu, this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
